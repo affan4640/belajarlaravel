6bbe9c34de7c9a396102ffb42726723d825213b6
affan4640 <affanridak@gmail.com>
2026-02-13 09:38:18 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view(uri:'/belajar', view:'/belajar');
+Route::view(uri:'/belajar2', view:'/belajar2');
